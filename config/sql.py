ac19e82d04a76931eec3e76fb8e5f0fdfd938abe
Suchao Ma <masuchao2023@163.com>
2025-07-07 00:29:23 +0800

    初始化项目框架

---
+++ config/sql.py
@@ -0,0 +1,14 @@
+class SQLConfig:
+    # Database connection details
+    DB_HOST = "localhost"
+    DB_port: 3306
+    DB_USER = "root"
+    DB_PASSWORD = "123456"
+    DB_NAME = "examAssistance"
+
+    #SQLAlchemy URL拼接
+    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_port}/{DB_NAME}"
+     # 关闭 SQLAlchemy 的修改追踪功能，节省内存
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 是否开启SQLAlchemy的调试日志
+    SQLALCHEMY_ECHO = True
